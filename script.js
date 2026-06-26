e18b2a17d50f8ac8dc99c4fa8ed44db651080a91
VIP <pavlomatviiv09@gmail.com>
2026-06-26 16:20:57 +0200

    Add registration and login functions in script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+// 🔐 Реєстрація
+function register() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let repeat = document.getElementById("repeat").value;
+    let msg = document.getElementById("msg");
+
+    if (!username || !password) {
+        msg.innerText = "Заповніть всі поля";
+        return;
+    }
+
+    if (password !== repeat) {
+        msg.innerText = "Паролі не співпадають";
+        return;
+    }
+
+    let user = {
+        username: username,
+        password: password,
+        rank: "Кадет",
+        car: "Немає"
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    msg.innerText = "Успішна реєстрація!";
+
+    setTimeout(() => {
+        window.location = "login.html";
+    }, 1000);
+}
+
+
+// 🔐 Вхід
+function login() {
+    let username = document.getElementById("loginUser").value;
+    let password = document.getElementById("loginPass").value;
+    let msg = document.getElementById("msg");
+
+    let user = JSON.parse(localStorage.getItem("user"));
+
+    if (!user) {
+        msg.innerText = "Користувача не знайдено";
+        return;
+    }
+
+    if (username === user.username && password === user.password) {
+        localStorage.setItem("logged", "true");
+        window.location = "profile.html";
+    } else {
+        msg.innerText = "Невірні дані";
+    }
+}
